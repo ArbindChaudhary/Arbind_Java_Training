6867e288b9030268b2c5ac1ff6b1415fbfdd5c7d
Gladstone <Gladstone@10.0.0.13>
2020-02-18 21:00:50 -0600

    Task5.6

---
+++ Task5.6/src/Programming.java
@@ -0,0 +1,18 @@
+public class Programming {
+
+
+
+	  public Programming(String s){
+	    System.out.println("I love "+s);
+	  }
+	  public Programming(){
+	    System.out.println("I love programming languages");
+	  }
+	
+
+	
+	  public static void main(String[] args){
+	    Programming s = new Programming("Java");
+	    Programming a = new Programming();
+	  }
+	}
